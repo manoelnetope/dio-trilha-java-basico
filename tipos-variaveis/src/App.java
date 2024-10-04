dffcc7d2c2085d2054b656bdc0d7f37b842b8da7
Manoel Marques <manoelneto_pe@hotmail.com>
2024-10-04 18:23:30 -0300

    tipos-variaveis

---
+++ tipos-variaveis/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+        
+
+
+    }
+}
