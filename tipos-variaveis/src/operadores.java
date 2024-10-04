407151debb2ed458e1732be4872516731f326728
Manoel Marques <manoelneto_pe@hotmail.com>
2024-10-04 18:23:45 -0300

    operadores.java

---
+++ tipos-variaveis/src/operadores.java
@@ -0,0 +1,8 @@
+public class operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "Linguagem" + " Java";
+        
+        System.out.println(nomeCompleto);
+     }
+
+}
