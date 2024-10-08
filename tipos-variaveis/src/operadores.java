9ebc4a66475e368a2e7454bc1755db4e2075c759
Manoel Marques <manoelneto_pe@hotmail.com>
2024-10-08 20:31:34 -0300

    Condição Ternaria

--- tipos-variaveis/src/operadores.java
+++ tipos-variaveis/src/operadores.java
@@ -1,11 +1,19 @@
 public class operadores {
+    
     public static void main(String[] args) {
+        int a, b;
+        a = 6;
+        b = 6;
+
+        String resultado = a==b ?"verdadeiro" : "falso";  
+
+        System.out.println(resultado);    
+            
+            
         
-        boolean variavel = true;
-
-        variavel = !variavel;
-
-        System.out.println(variavel);
+            
+        
+         
     
 
      }
